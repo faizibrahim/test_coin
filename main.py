071e27682f0f56fa539b6b894cc82a45773d2db7
jibybabu <jiby.babu@uconn.edu>
2017-06-14 20:39:35 -0700

    -- just a test change

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 def main():
     ethereum = CurrencyData()
     data = ethereum.get_data("06/10/2017-20:00:00","06/10/2017-23:00:00")
+
     #data = ethereum.get_data_for_last_hour()
+
     print(data)
     x,y = zip(*data)
     plt.plot(x,y)
